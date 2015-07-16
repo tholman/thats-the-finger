fadfb98a174d2fc4be912cbdf84a39bc3118fcd7
Tim <timothy.w.holman@gmail.com>
2015-07-15 22:40:51 -0400

    initial commit

---
+++ index.html
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+    <head>
+        <title> The finger, deal with it. </title>
+
+        <!-- CSS -->
+        <link href='./css/styles.css' rel='stylesheet'>
+
+        <!-- META -->
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        <meta name="description" content="Yep, thats the finger!">
+        <meta property="og:title" content="The finger, deal with it."/>
+        <meta property="og:site_name" content="Thats the Finger"/>
+        <meta property="og:description" content="Hey you betta check this out... yep, you deserve it."/>
+        <meta property="og:image" content="http://thatsthefinger.com/img/share.png"/>
+        <meta property="article:author" content="https://www.facebook.com/guy.trefler" />
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Yep, thats the finger.">
+        <meta name="twitter:description" content="... Deal with it.">
+        <meta name="twitter:image" content="http://thatsthefinger.com/img/share.png">
+
+    </head>
+    <body>
+    	<div class="hand"></div>
+    	<div class="share">
+            <a href="https://twitter.com/share" class="twitter-share-button" data-text="The finger, yep, deal with it... via @guytrefler & @twholman -" data-align="right">Tweet</a>
+            <div class="fb-like" data-href="http://thatsthefinger.com" data-send="false" data-layout="button_count" data-width="200" data-show-faces="false"></div>
+    	</div>
+        <script src='js/flip-the-bird.js'></script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-56795519-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
+    </body>
+</html>
---
+++ js/flip-the-bird.js
@@ -0,0 +1,75 @@
+/**
+ * What, code!?
+ */
+
+// SO GLOBAL. YES!
+var damnItsTheBody = document.body;
+var totalHeight = window.innerHeight;
+var color1 = 'rgb(204, 204, 204)'; // super grey
+var color2 = 'rgb(255, 66, 66)';   // super red!
+
+var theWidth = 8256; // Pixels, fuck!
+var theFrames = 24;
+var thatWidth = theWidth / theFrames
+var totalHeight = window.innerHeight;
+
+var hand = document.querySelector( '.hand' );
+
+function App() {
+
+    this.init = function() {
+
+    	// Wait! What! Someone Should trottle this! What! No! Damn!
+		window.onmousemove = function( event ) {
+
+			var percentage = 1 - ( event.y / totalHeight );
+			var color = fadeToColor(color1, color2, percentage)
+			damnItsTheBody.style.backgroundColor = color;
+
+			var segment = Math.floor( 24 * percentage );
+
+			// Wrangle those final frames!
+			if ( segment > 23 ) {
+    			segment = 23;
+  			} else if ( segment < 0 ) {
+  				segment = 0;
+  			}
+
+  			hand.style.backgroundPosition = ((-segment * thatWidth) + 'px 0px');
+		}
+
+		window.onresize = function() {
+			totalHeight = window.innerHeight;
+		}
+
+   		// Load Twitter
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+        // Load Facebook
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+    }
+
+    // Stack overflow is amazing! Smart! Kapow!
+	function fadeToColor(rgbColor1, rgbColor2, ratio) {
+	    var color1 = rgbColor1.substring(4, rgbColor1.length - 1).split(','),
+	        color2 = rgbColor2.substring(4, rgbColor2.length - 1).split(','),
+	        difference,
+	        newColor = [];
+
+	    for (var i = 0; i < color1.length; i++) {
+	        difference = color2[i] - color1[i];
+	        newColor.push(Math.floor(parseInt(color1[i], 10) + difference * ratio));
+	    }
+
+	    return 'rgb(' + newColor + ')';
+	}
+}
+
+var app = new App();
+app.init();
